8bf260fe38aa0984aef68b621f47a962cafbd929
syahrilhanla <syahrilhanla5@gmail.com>
2022-06-11 07:39:17 +0800

    fixing up edges

---
+++ pages/article/[id]/index.js
@@ -0,0 +1,10 @@
+import { useRouter } from "next/router";
+
+const article = () => {
+	const router = useRouter();
+	const { id } = router.query;
+
+	return <div>this is an article {id}</div>;
+};
+
+export default article;
