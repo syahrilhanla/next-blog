d11497b556dbdb16174e1bb29b4e643c9f184bdf
syahrilhanla <syahrilhanla5@gmail.com>
2022-06-11 07:38:58 +0800

    added article components

---
+++ components/ArticleList.jsx
@@ -0,0 +1,14 @@
+import articleStyles from "../styles/Article.module.css";
+import ArticleItem from "./ArticleItem";
+
+const ArticleList = ({ articles }) => {
+	return (
+		<div className={articleStyles.grid}>
+			{articles.map((article) => {
+				return <ArticleItem article={article} />;
+			})}
+		</div>
+	);
+};
+
+export default ArticleList;
