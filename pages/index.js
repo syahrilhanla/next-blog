5a38f420fb30c022d75c63707bb057cc255f80a6
syahrilhanla <syahrilhanla5@gmail.com>
2022-06-11 08:39:16 +0800

    added api for fetching data from dummy articles

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,7 @@ export default function Home({ articles }) {
 }
 
 export const getStaticProps = async () => {
-	const res = await fetch(
-		"https://jsonplaceholder.typicode.com/posts?_limit=6"
-	);
+	const res = await fetch("http://localhost:3000/api/articles");
 	const articles = await res.json();
 
 	return {
