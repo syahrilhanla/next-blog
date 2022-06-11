606fe35d1672ec94a934fe48f8b2235750d50930
syahrilhanla <syahrilhanla5@gmail.com>
2022-06-11 08:38:47 +0800

    added data for api fetch trying

---
+++ data.js
@@ -0,0 +1,29 @@
+export const articles = [
+	{
+		id: "1",
+		title: "Umur semakin diujung tanduk, Fritz tetap memojokkan ...",
+		excerpt:
+			"GitHub today announced a bunch of new features at its virtual GitHub...",
+		body: "Semakin pendeknya usia merupakan sebuah hal yang pasti dialami oleh semua makhluk hidup. Namun dengan fakta tersebut tidak menghentikan Fritz, seorang penguasa lalim untuk tetap mengeruk semua keuntungan yang dapat dia raih dari sumber daya manusia yang sedang mengalami krisis kehidupan",
+	},
+	{
+		id: "2",
+		title: "Apa yang menyebabkan krisis masa muda Plokom?",
+		excerpt: "Most developers don’t care about multi-cloud. But they should...",
+		body: "Demografi usia di sebuah prodi, Pendidikan Komputer, terbilang cukup miring. Pasalnya dengan data kurva usia yang sedemikian rupa mendeskripsikan bagaimana kualitas dari sumber daya yang ada di prodi tersebut.",
+	},
+	{
+		id: "3",
+		title: "Here is how to make your website more accessible",
+		excerpt:
+			"An accessible website is one that’s optimized for all people, including those with disabilities...",
+		body: "There are many things to consider when setting up a website, and accessibility is one factor that can sometimes be overlooked. An accessible website is one that’s optimized for all people, including those with impaired vision or hearing, motor difficulties, or learning disabilities",
+	},
+	{
+		id: "4",
+		title: "Why open ecosystems are the future of app development",
+		excerpt:
+			"When app stores entered the mainstream tech culture, they exposed developers to an audience of millions...",
+		body: "We can’t get enough of our mobile apps. There were 204 billion apps downloads last year, and that number is rising in 2020.  When app stores entered the mainstream tech culture, they exposed developers to an audience of millions who were keen to adopt the innovative capabilities",
+	},
+];
